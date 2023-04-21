62a9a9776e6310ff1d8cd433234c48ee8c8e367a
unknown <anuragsonkar737@gmail.com>
2023-04-22 00:51:43 +0530

    4th commit

--- server/App.js
+++ server/App.js
@@ -8,12 +8,21 @@ const app = express();
 dotenv.config({ path: './config.env'});
 require('./db/conn');
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use(express.json());
 //const User = require('./model/userSchema');
 app.use(require('./routers/auth'));
 const PORT = process.env.PORT||5000;
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+})
+}
+    
+    
+
 
 console.log("hello Anurag");
 app.listen(PORT,()=>{
