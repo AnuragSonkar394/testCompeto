38b1502c64b3586569aa9fb5648a190971139253
unknown <anuragsonkar737@gmail.com>
2023-04-22 17:41:43 +0530

    6th commit

--- server/App.js
+++ server/App.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(require('./routers/auth'));
 const PORT = process.env.PORT||5000;
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static('client/build'));
     app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    res.sendFile(path.resolve(__dirname,'./client','build','index.html'));
 })
 }
     
